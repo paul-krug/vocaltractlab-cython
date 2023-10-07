7eed7c049cb1ac710412b0b59d04ff3d5162486c
paul-krug <paul_konstantin.krug@tu-dresden.de>
2023-10-07 18:41:59 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,17 +1,32 @@
 import os, sys
-import logging
+#import logging
 import subprocess
 import shutil
 from setuptools import setup
 from setuptools.extension import Extension
 from Cython.Build import cythonize
-import numpy as np
+#import numpy as np
 
 API_NAME = 'VocalTractLabApi'
 WORKING_PATH = os.getcwd()
+VTL_CYTHON_PATH = os.path.join( WORKING_PATH, 'vocaltractlab_cython' )
 BACKEND_PATH = os.path.join( 'VocalTractLabBackend-dev' )
 BUILD_PATH = os.path.join( BACKEND_PATH, 'out' )
 API_INC_PATH = os.path.join( BACKEND_PATH, 'include', API_NAME )
+if sys.platform == 'win32':
+    LIB_PATH = os.path.join(
+        BACKEND_PATH,
+        'lib',
+        'Release',
+        'VocalTractLabApi.dll',
+        )
+else:
+    LIB_PATH = os.path.join(
+        BACKEND_PATH,
+        'lib',
+        'Release',
+        'libVocalTractLabApi.so',
+        )
 
 cmd_config = [
     'cmake',
@@ -39,32 +54,20 @@ def build_vtl_api():
     subprocess.check_call( cmd_config )
     subprocess.check_call( cmd_build )
 
-
-    #api_name = 'VocalTractLabApi'
-    #if sys.platform == 'win32':
-    #    file_extension = '.dll'
-    #    shutil.copy( os.path.join( 'Release', api_name + file_extension ), os.path.join( WORKING_PATH, 'VocalTractLab' ) )
-    #    shutil.copy( os.path.join( 'Release', api_name + '.lib' ), os.path.join( WORKING_PATH, 'VocalTractLab' ) )
-    #else:
-    #    file_extension = '.so'
-    #    try:
-    #        shutil.move( 'lib' + api_name + file_extension, '/usr/local/lib/' )
-    #    except Exception:
-    #        print( 'WARNING: Could not move libVocalTractLabApi to standard location /usr/local/lib/ ' +
-    #            ' Make shure you have permission or manually move the file to an appropriate location.' +
-    #            'File is located at {}'.format( os.path.join( WORKING_PATH, 'libVocalTractLabApi.so' ) ) )
-
     os.chdir( WORKING_PATH )
+        
+    # Copy the library file from backend to vtl_cython
+    shutil.copy(
+        LIB_PATH,
+        VTL_CYTHON_PATH,
+        )
     # Copy API header file from backend to vtl_cython
     shutil.copy(
         os.path.join(
             API_INC_PATH,
             API_NAME + '.h',
             ),
-        os.path.join(
-            WORKING_PATH,
-            'vocaltractlab_cython',
-            )
+        VTL_CYTHON_PATH,
         )
     # Copy the resource folder from backend to vtl_cython
     shutil.copytree(
@@ -73,14 +76,13 @@ def build_vtl_api():
             'resources',
             ),
         os.path.join(
-            WORKING_PATH,
-            'vocaltractlab_cython',
+            VTL_CYTHON_PATH,
             'resources',
             )
         )
     
     # Delete the build folder
-    shutil.rmtree( BUILD_PATH )
+    #shutil.rmtree( BUILD_PATH )
     return
 
 
@@ -94,7 +96,8 @@ def build_vtl_api():
 #    #runtime_library_dirs=runtime_library_dirs #'./', './VocalTractLab/', './VocalTractLab/VocalTractLabApi' ],
 #    )
 
-#build_vtl_api()
+build_vtl_api()
+stop
 
 if sys.platform == 'win32':
     runtime_library_dirs = None
